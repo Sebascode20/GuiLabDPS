f3de663fe992f6e491df62ab64717ad71dc158be
Sebascode20 <sebastiancrack20@outlook.com>
2025-03-29 16:30:05 -0600

    Agregando procedimiento guia 8

---
+++ GuiLabDPS/Guia08/navegacion/src/screens/AboutScreen.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { View, Text } from 'react-native';
+
+export default function AboutScreen(){
+    return(
+        <View>
+            <Text>Estamos en About</Text>
+        </View>
+    );
+}
---
+++ GuiLabDPS/Guia08/navegacion/src/screens/ContactScreen.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { View, Text } from 'react-native';
+
+export default function ContactScreen(){
+    return(
+        <View>
+            <Text>Estamos en Contact</Text>
+        </View>
+    );
+}
